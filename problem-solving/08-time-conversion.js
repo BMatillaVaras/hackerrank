1d27eb69a558a007cb56f99f7a1e32a7ca10d246
BMatillaVaras <bmatillavaras@gmail.com>
2021-03-04 13:54:15 +0100

    refactor 08 problem solving

--- problem-solving/08-time-conversion.js
+++ problem-solving/08-time-conversion.js
@@ -38,31 +38,12 @@ function timeConversion(s) {
       return s.replace("12", "00").slice(0, -2);
     }
     return s.slice(0, -2);
-  } else if (s.includes("PM")) {
+  } else {
+    let newHour = hour + 12;
     if (hour === 12) {
       return s.slice(0, -2);
-    } else if (hour === 1) {
-      return s.replace("01", "13").slice(0, -2);
-    } else if (hour === 2) {
-      return s.replace("02", "14").slice(0, -2);
-    } else if (hour === 3) {
-      return s.replace("03", "15").slice(0, -2);
-    } else if (hour === 4) {
-      return s.replace("04", "16").slice(0, -2);
-    } else if (hour === 5) {
-      return s.replace("05", "17").slice(0, -2);
-    } else if (hour === 6) {
-      return s.replace("06", "18").slice(0, -2);
-    } else if (hour === 7) {
-      return s.replace("07", "19").slice(0, -2);
-    } else if (hour === 8) {
-      return s.replace("08", "20").slice(0, -2);
-    } else if (hour === 9) {
-      return s.replace("09", "21").slice(0, -2);
-    } else if (hour === 10) {
-      return s.replace("10", "22").slice(0, -2);
-    } else if (hour === 11) {
-      return s.replace("11", "23").slice(0, -2);
+    } else {
+      return s.replace(s.slice(0, 2), newHour).slice(0, -2);
     }
   }
 }
