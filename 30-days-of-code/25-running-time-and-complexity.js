e5bd7f1c617b1c4208f3dc0c3fcaf65f584be0a0
BMatillaVaras <bmatillavaras@gmail.com>
2021-03-21 15:17:37 +0100

    add day 25 of 30 days of code

---
+++ 30-days-of-code/25-running-time-and-complexity.js
@@ -0,0 +1,42 @@
+function processData(input) {
+  var inputArray = input.split("\n").map(Number);
+
+  // removes the first element that is not required below
+  inputArray.shift();
+
+  inputArray.forEach(function (element) {
+    if (isPrime(element)) {
+      console.log("Prime");
+    } else {
+      console.log("Not prime");
+    }
+  });
+}
+
+function isPrime(number) {
+  var range = Math.floor(Math.sqrt(number));
+  if (number < 2) {
+    return false;
+  } else if (number === 2) {
+    return true;
+  }
+
+  for (var i = 2; i <= range; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
