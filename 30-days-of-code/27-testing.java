390e14fc2bab30701ea2ec688dc6d8d172107d28
BMatillaVaras <bmatillavaras@gmail.com>
2021-03-29 13:00:43 +0200

    add day 27 of 30 days of code

---
+++ 30-days-of-code/27-testing.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+public class Solution {
+
+     static class TestDataEmptyArray {
+        public static int[] get_array() {
+            int[] arr = {};
+            return arr; 
+        }
+    }
+
+    static class TestDataUniqueValues {
+        
+        static int[] arr = {1,2,3,4};
+
+        public static int[] get_array() {
+            return arr; 
+        }
+
+        public static int get_expected_result() {
+            return 0; 
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+
+        static int[] arr = {0,0,1}; 
+
+        public static int[] get_array() {
+            return arr; 
+        }
+
+        public static int get_expected_result() {
+            return 0; 
+        }
+    }
+
+    
+	public static void TestWithEmptyArray() {
+        try {
+            int[] seq = TestDataEmptyArray.get_array();
+            int result = minimum_index(seq);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+        throw new AssertionError("Exception wasn't thrown as expected");
+    }
+
+    public static void TestWithUniqueValues() {
+        int[] seq = TestDataUniqueValues.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        Integer[] tmp = new Integer[seq.length];
+        for (int i = 0; i < seq.length; ++i) {
+            tmp[i] = Integer.valueOf(seq[i]);
+        }
+        if (!((new LinkedHashSet<Integer>(Arrays.asList(tmp))).size() == seq.length)) {
+            throw new AssertionError("not all values are unique");
+        }
+
+        int expected_result = TestDataUniqueValues.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void TestWithExactlyTwoDifferentMinimums() {
+        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        int[] tmp = seq.clone();
+        Arrays.sort(tmp);
+        if (!(tmp[0] == tmp[1] && (tmp.length == 2 || tmp[1] < tmp[2]))) {
+            throw new AssertionError("there are not exactly two minimums in the array");
+        }
+
+        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestWithEmptyArray();
+        TestWithUniqueValues();
+        TestWithExactlyTwoDifferentMinimums();
+        System.out.println("OK");
+    }
+}
